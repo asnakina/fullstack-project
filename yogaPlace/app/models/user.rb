5ec02b5ae7503d7765ca3ba5cd9999d2963d9c2b
asnakina <asnakina1@gmail.com>
2019-01-04 22:54:16 -0500

    some changes in README

--- yogaPlace/app/models/user.rb
+++ yogaPlace/app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
+  has_many :reviews, dependent: :nullify
   has_secure_password
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true
 
   def to_token_payload
     {
