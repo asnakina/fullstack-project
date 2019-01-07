6c29570bbeec79b1eb8866fab77f461704937d71
asnakina <asnakina1@gmail.com>
2019-01-07 15:19:49 -0500

    LoginForm component is done

---
+++ yogaPlace/client/src/components/LoginForm.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function LoginForm(props) {
+  return (
+    <form onSubmit={props.handleLogin}>
+        <h2>Login</h2>
+        <h3>Email:</h3>
+        <input onChange={props.handleChange}
+               name="email"
+               value={props.login.email} />
+        <br><br>
+        <h3>Password:</h3>
+        <input onChange={props.handleChange}
+               name="password"
+               value={props.login.password} />
+        <br><br>
+        <button type="submit">Login</button>
+    <form>
+  )
+}
+
+export default LoginForm;
