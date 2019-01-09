849c34e325e44a427aa94c89768f2564cac19e2b
asnakina <asnakina1@gmail.com>
2019-01-08 20:18:05 -0500

    Classes Component started

--- yogaPlace/client/src/components/Classes/ClassesView.js
+++ yogaPlace/client/src/components/Classes/ClassesView.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
 import {getAllClasses} from '../services/classes';
+import {getAllLocations} from '../services/locations';
 import '../App.css';
 
 export default class ClassesView extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      classes: []
+      classes: [],
+      locations: []
     }
     this.getClasses = this.getClasses.bind(this);
+    this.getLocations = this.getLocations.bind(this);
   }
 
   async componentDidMount() {
-    this.getClasses()
+    this.getClasses();
+    this.getLocations()
   }
 
 //Get
@@ -21,6 +25,16 @@ export default class ClassesView extends Component {
       const classes = await getAllClasses();
     //const classes = await services.getAllClasses();
        this.setState({classes});
+    } catch(e) {
+      console.error(e);
+    }
+  }
+
+//Get
+  async getLocations() {
+    try {
+      const locations = await getAllLocations();
+       this.setState({locations});
     } catch(e) {
       console.error(e);
     }
@@ -38,6 +52,16 @@ export default class ClassesView extends Component {
    render() {
      return (
        <div>
+        <h2>Locations:</h2>
+        <div className="AllLocations">
+          {this.state.locations.map(theLocation => (
+             <div key={theLocation.id}>
+               <h3>{theLocation.name}</h3>
+               <h3>{theLocation.address}</h3>
+               <h3>{theLocation.lat}{theLocation.lng}</h3>
+               <br></br>
+               <div classes={this.state.classes.filter(theClass => the_class.id === the_class_locations_['the_class_id'])}/>
+            </div>
           <h2>Classes:</h2>
             <div className="AllClasses">
               {this.state.classes.map(oneClass => (
