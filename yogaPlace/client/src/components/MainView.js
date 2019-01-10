4213046e1d834a55c2d0aaf0f882eba42a62bc8c
asnakina <asnakina1@gmail.com>
2019-01-10 15:58:50 -0500

    linked words on main page are done

--- yogaPlace/client/src/components/MainView.js
+++ yogaPlace/client/src/components/MainView.js
@@ -1,17 +1,44 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 function MainView (props) {
   return (
-    <div className="mainClass">
-      {/*<a href=component={AboutView}>   </a>*/}
-      <h2 className="breathingLines" id="exercisesText">Exercises</h2>
-      <h2 className="breathingLines" id="scheduleText">Schedule</h2>
-      <h2 className="breathingLines" id="aboutText">About</h2>
-      <h2 className="breathingLines" id="retreatsText">Retreats</h2>
-      <h2 className="breathingLines" id="reviewsText">Reviews</h2>
-      <h2 className="breathingLines" id="profileText">Profile</h2>
-    </div>
-  )
+       <div className="mainClass">
+         {/*<a href=component={AboutView}>   </a>*/}
+         <ul>
+           <li>
+             <Link to="/exercises">
+               <h2 className="breathingLines" id="exercisesText">Exercises</h2>
+             </Link>
+            </li>
+            <li>
+             <Link to="/schedule">
+               <h2 className="breathingLines" id="scheduleText">Schedule</h2>
+             </Link>
+            </li>
+            <li>
+             <Link to="/about">
+               <h2 className="breathingLines" id="aboutText">About</h2>
+             </Link>
+            </li>
+            <li>
+             <Link to="/retreats">
+               <h2 className="breathingLines" id="retreatsText">Retreats</h2>
+             </Link>
+            </li>
+            <li>
+             <Link to="/reviews">
+              <h2 className="breathingLines" id="reviewsText">Reviews</h2>
+              </Link>
+            </li>
+            <li>
+              <Link to="/profile">
+               <h2 className="breathingLines" id="profileText">Profile</h2>
+              </Link>
+            </li>
+         </ul>
+      </div>
+   )
 }
 
 export default MainView;
