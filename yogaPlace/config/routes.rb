7c4b0655ea2ff69e3b4bf99c1344bd443d9a072f
asnakina <asnakina1@gmail.com>
2019-01-14 16:03:13 -0500

    some changes with routes, set up fallback to index

--- yogaPlace/config/routes.rb
+++ yogaPlace/config/routes.rb
@@ -7,4 +7,7 @@ Rails.application.routes.draw do
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
  end
+ get '*path', to: "application#fallback_index_html", constraints: ->(request) do
+  !request.xhr? && request.format.html?
+ end
 end
